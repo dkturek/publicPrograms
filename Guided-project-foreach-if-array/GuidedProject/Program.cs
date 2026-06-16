7c7c41b05c02feb6c98c201fa94ffff2f9398b36
dkturek <dkturek@yahoo.com>
2026-06-16 16:07:38 -0400

    Update Public version with Sophia array
    Sophia array added

--- Guided-project-foreach-if-array/GuidedProject/Program.cs
+++ Guided-project-foreach-if-array/GuidedProject/Program.cs
@@ -8,24 +8,28 @@ int sophia2 = 86;
 int sophia3 = 87;
 int sophia4 = 98;
 int sophia5 = 100;
+int[] sophiaScores = new int[] {sophia1, sophia2, sophia3, sophia4, sophia5};
 
 int andrew1 = 92;
 int andrew2 = 89;
 int andrew3 = 81;
 int andrew4 = 96;
 int andrew5 = 90;
+int[] andrewScores = new int[] {andrew1, andrew2, andrew3, andrew4, andrew5};
 
 int emma1 = 90;
 int emma2 = 85;
 int emma3 = 87;
 int emma4 = 98;
 int emma5 = 68;
+int[] emmaScores = new int[] {emma1, emma2, emma3, emma4, emma5};
 
 int logan1 = 90;
 int logan2 = 95;
 int logan3 = 87;
 int logan4 = 88;
 int logan5 = 96;
+int [] loganScores = new int[] {logan1, logan2, logan3, logan4, logan5};
 
 int sophiaSum = 0;
 int andrewSum = 0;
@@ -37,18 +41,39 @@ decimal andrewScore;
 decimal emmaScore;
 decimal loganScore;
 
-sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
+// Student names
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+
+foreach (string name in studentNames)
+{
+    if (name == "Sophia")
+    {
+        foreach (int score in sophiaScores)
+        {
+            // add the exam score to the sum
+            sophiaSum += score;
+        }
+
+        sophiaScore = (decimal)(sophiaSum) / currentAssignments;
+
+        Console.WriteLine("Student\t\tGrade\n");
+        Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+    }
+}
+
+
+//sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
 andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
 emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
 loganSum = logan1 + logan2 + logan3 + logan4 + logan5;
 
-sophiaScore = (decimal)sophiaSum / currentAssignments;
+//sophiaScore = (decimal)sophiaSum / currentAssignments;
 andrewScore = (decimal)andrewSum / currentAssignments;
 emmaScore = (decimal)emmaSum / currentAssignments;
 loganScore = (decimal)loganSum / currentAssignments;
 
-Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+//Console.WriteLine("Student\t\tGrade\n");
+//Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
 Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
 Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
 Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
