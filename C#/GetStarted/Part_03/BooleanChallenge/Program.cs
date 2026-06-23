5fa4f1a4c13abd098994a2a0fb88b604a10939b8
dkturek <dkturek@yahoo.com>
2026-06-22 20:42:27 -0400

    Updating what is in Part 3 so far
    not sure if I should move this down a level. At least added a gitignore.

---
+++ C#/GetStarted/Part_03/BooleanChallenge/Program.cs
@@ -0,0 +1,51 @@
+﻿//string permission = "Admin|Manager";
+//int level = 55;
+
+
+Console.Write("Enter your permission level (Admin or Manager): ");
+string userPermission = Console.ReadLine() ?? "Default";
+if (userPermission == "" || userPermission == null)
+{
+    userPermission = "Default";
+}
+
+Console.Write("Enter your level: ");
+string userLevelInput = Console.ReadLine() ?? "0";
+int userLevel = 0;
+if (userLevelInput == "")
+    userLevel = 0;  
+else if (userLevelInput != null)
+    userLevel = int.Parse(userLevelInput);
+
+
+
+if (userPermission == "Admin")
+    { 
+        if (userLevel > 55)
+        {
+            Console.WriteLine("Welcome, Super Admin user.");
+        }
+        else 
+        {
+            Console.WriteLine("Welcome, Admin user.");
+        }
+    }
+
+else if (userPermission == "Manager")
+    {
+        if (userLevel >= 20)
+        {
+            Console.WriteLine("Contact an Admin for access.");
+        }
+        else 
+        {
+            Console.WriteLine("You do not have sufficient privileges.");
+        }
+    }
+else
+{
+    Console.WriteLine($"You do not have sufficient privileges as {userPermission}.");
+}
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
---
+++ C#/GetStarted/Part_03/ConditionalChallenge/Program.cs
@@ -0,0 +1,4 @@
+﻿Random coin = new Random();
+int coinValue = coin.Next(0, 2);
+Console.WriteLine((coinValue == 0) ? "Heads" : "Tails");
+
