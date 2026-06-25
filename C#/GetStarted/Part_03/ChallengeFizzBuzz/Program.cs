dd453f07b1166cc5cb3a45cb62efefc5de3d14bb
dkturek <dkturek@yahoo.com>
2026-06-24 20:20:27 -0400

    CodeBlocks
    Used StringBuilder rather than making strings in the loop.

---
+++ C#/GetStarted/Part_03/ChallengeFizzBuzz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+StringBuilder suffix = new StringBuilder();
+
+for (int i = 1; i <= 100; i++)
+{
+    //string suffix = "";
+    suffix.Clear();
+    if (i % 3 == 0)
+        suffix.Append("Fizz");
+    if (i % 5 == 0)
+        suffix.Append("Buzz");
+    
+    if (suffix.Length == 0)
+        Console.WriteLine(i);
+    else
+        Console.WriteLine($"{i} - {suffix}");    
+}
+
+/*
+Checked this one with AI to see if defining suffix used less resources
+inside the loop or outside. While we both agreed inside the loop is best,
+the AI also suggested using StringBuilder instead of string concatenation for better performance.
+and that would have to be defined outside the loop.
+*/
---
+++ C#/GetStarted/Part_03/CodeBlockScope/Program.cs
@@ -0,0 +1,21 @@
+﻿bool flag = true;
+int value = 0;
+if (flag)
+{
+    Console.WriteLine($"Inside the code block: {value}");
+}
+value = 10;
+Console.WriteLine($"Outside the code block: {value}");
+
+bool flag2 = true;
+if (flag2) 
+    Console.WriteLine(flag2);
+
+string name = "steve";
+
+if (name == "bob")
+    Console.WriteLine("Found Bob");
+else if (name == "steve") 
+    Console.WriteLine("Found Steve");
+else
+    Console.WriteLine("Found Chuck");
